c10a50bb972148e5e42b80a18620141fe9896bca
researchersec <109282836+researchersec@users.noreply.github.com>
2024-03-19 15:34:11 +0100

    Update parse_demo.py

--- parse_demo.py
+++ parse_demo.py
@@ -20,16 +20,27 @@ def parse_demo_file():
             # Parse player_death event for players X and Y
             event_df = parser.parse_event("player_death", player=["X", "Y"], other=["total_rounds_played"])
             # Parse ticks for players X and Y
-            ticks_df = parser.parse_ticks(["X", "Y"])
+            #ticks_df = parser.parse_ticks(["X", "Y"])
 
             # Compute hash of the .dem file
             file_hash = compute_file_hash(file)
 
             # Save event_df and ticks_df to JSON files in their respective folders
             os.makedirs('events', exist_ok=True)
-            os.makedirs('ticks', exist_ok=True)
+            os.makedirs('convars', exist_ok=True)
+            os.makedirs('grenades', exist_ok=True)
+            os.makedirs('chat', exist_ok=True)
+            os.makedirs('player_info', exist_ok=True)
+            os.makedirs('item_drops', exist_ok=True)
+            os.makedirs('skins', exist_ok=True)
+            
             event_df.to_json(f'events/{file_hash}.json', indent=4)
-            ticks_df.to_json(f'ticks/{file_hash}.json', indent=4)
+            pd.json_normalize(convars).to_json(f'convars/{demo_id}.json', indent=4)
+            grenades.to_json(f'grenades/{demo_id}.json', indent=4)
+            chat.to_json(f'chat/{demo_id}.json', indent=4)
+            player_info.to_json(f'player_info/{demo_id}.json', indent=4)
+            item_drops.to_json(f'item_drops/{demo_id}.json', indent=4)
+            skins.to_json(f'skins/{demo_id}.json', indent=4)
 
 if __name__ == "__main__":
     parse_demo_file()
